e83cbad1fd28ac1ebe02d21368b20e1bf69fd9e7
Eric Babcock <eric.babcock@protonmail.com>
2024-03-29 18:20:00 +0100

    refactored-packages

--- SonarcloudDemo/SonarcloudDemo-ejb/src/java/sonarqube/dao/UserAccess.java
+++ SonarcloudDemo/SonarcloudDemo-ejb/src/java/sonarqube/dao/UserAccess.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package de.uni_koblenz.adv_topics.sonarqube.entities;
+package sonarqube.dao;
 
 /**
  *
  * @author Eric Babcock <ebabcock@uni-koblenz.de>
  */
+import entity.UserEntity;
+import sonarqube.dao.AbstractAccess;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
